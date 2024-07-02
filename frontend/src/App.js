41e40ccdec26fa31e44cd395636ece71bfcb1094
Harshith KV <harshith.shetty2002@gmail.com>
2024-07-02 14:24:50 +0530

    added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignInSignUp from './components/RegisterForm';
+
+function App() {
+  return (
+    <div>
+      <SignInSignUp/>
+    </div>
+  );
+}
+
+export default App;
